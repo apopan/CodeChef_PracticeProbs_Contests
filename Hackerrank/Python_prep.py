97f8b34b1139bc55a8e7b8f6a9c3db70a0485467
Apoorv Pandit <82618041+apopan@users.noreply.github.com>
2023-02-21 23:25:06 +0530

    Update Python_prep.py

--- Hackerrank/Python_prep.py
+++ Hackerrank/Python_prep.py
@@ -35,6 +35,15 @@ if __name__ == '__main__':
     print(a+b)
     print(a-b)
     print(a*b)
+    
+
+# Print Function
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        i=i+1
+        print(i, end="")
 
 
 
