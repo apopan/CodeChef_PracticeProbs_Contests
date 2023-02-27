f94cd5740d6135f16c40f832643d32be34650041
Apoorv Pandit <82618041+apopan@users.noreply.github.com>
2023-02-27 22:48:33 +0530

    Update Python_prep.py

--- Hackerrank/Python_prep.py
+++ Hackerrank/Python_prep.py
@@ -62,3 +62,22 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(n):
         print(i*i)
+        
+      
+# Write a function
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year%400 == 0:
+        leap = True
+    elif year%100 == 0:
+        leap = False 
+    elif year%4 == 0:
+        leap = True
+         
+    return leap
+
+year = int(input())
+print(is_leap(year))
