9e16eabf86a44e6fa60e94a57c5fa03724183623
Apoorv Pandit <82618041+apopan@users.noreply.github.com>
2023-07-28 20:19:53 +0530

    Create Masterchef_finals.py

---
+++ Practice_problems/Masterchef_finals.py
@@ -0,0 +1,11 @@
+# Code = TOP10
+# Name = Masterchef finals
+
+T = int(input())
+for i in range(T):
+    X = int(input())
+    if X <=10:
+        print("YES")
+    else:
+        print("NO")
+      
