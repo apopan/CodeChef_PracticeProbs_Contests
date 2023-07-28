ce1de1b063152d8ccffb688fd4f8e50270b0bdaf
Apoorv Pandit <82618041+apopan@users.noreply.github.com>
2023-07-28 22:03:16 +0530

    Create Tom_and_Jerry_Chase.py

---
+++ Practice_problems/Tom_and_Jerry_Chase.py
@@ -0,0 +1,11 @@
+# Code : JERRYCHASE
+# Name : Tom and Jerry Chase 
+
+for i in range(T):
+    X, Y = list(map(int,input().split()))
+    if X < Y:
+        print("YES")
+    else:
+        print("NO")
+
+  
