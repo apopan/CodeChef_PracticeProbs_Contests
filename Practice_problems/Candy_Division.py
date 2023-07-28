23ba6ac010a4e21c33b73679464c8805317bb85e
Apoorv Pandit <82618041+apopan@users.noreply.github.com>
2023-07-28 20:24:29 +0530

    Create Candy_Division.py

---
+++ Practice_problems/Candy_Division.py
@@ -0,0 +1,11 @@
+# Code = CANDIVIDE
+# Name = Candy Division
+
+T = int(input())
+for i in range(T):
+    N = int(input())
+    if N % 3 == 0:
+        print("YES")
+    else:
+        print("NO")
+      
